7884eeb3ff1a5ee91e0ea11e68ba8be1c4c064fc
IOtomo <s2001024@sendai-nct.jp>
2023-08-21 13:29:35 +0900

    簡易UIの作成

---
+++ public/update/code.js
@@ -0,0 +1,69 @@
+// JSONデータの読み込み
+import data from './test.json' assert { type: 'json' };
+
+// ユーザー情報を表示
+const userNameElement = document.getElementById('userName');
+userNameElement.textContent = data.user;
+
+// ユーザーの進捗をプログレスバーで表示
+const userProgress = document.getElementById('userProgress');
+userProgress.value = data.completed;
+
+// JSONデータからタスクを動的に生成
+const taskList = document.getElementById('taskList');
+data.tasks.forEach((task) => {
+  const taskSet = createTaskSet(task);
+  taskList.appendChild(taskSet);
+});
+
+// タスクを更新する関数
+function updateTask(taskNumber) {
+  // タスク内容の取得
+  const taskContent = document.querySelector(
+    `.taskSet:nth-child(${taskNumber}) .taskTitle`
+  ).textContent;
+
+  const newValue = document.querySelector(
+    `.taskSet:nth-child(${taskNumber}) .newValue`
+  ).checked;
+
+  // 更新結果を表示するエリアにメッセージを表示
+  const updateResult = document.getElementById('updateResult');
+  updateResult.textContent = `タスク "${taskContent}" が更新されました。新しい値: ${newValue}`;
+}
+
+// タスクセットを生成する関数
+function createTaskSet(task) {
+  const taskSet = document.createElement('div');
+  taskSet.classList.add('taskSet');
+
+  const taskTitle = document.createElement('p');
+  taskTitle.classList.add('taskTitle');
+  taskTitle.textContent = `タスク${task.id}: ${task.taskName}`;
+
+  const completed = document.createElement('p');
+  completed.classList.add('completed');
+  completed.textContent = task.completed ? '完了済み' : '未完了';
+
+  const label = document.createElement('label');
+  label.textContent = '新しい値:';
+
+  const newValueInput = document.createElement('input');
+  newValueInput.type = 'checkbox';
+  newValueInput.classList.add('newValue');
+  newValueInput.required = true;
+
+  const updateButton = document.createElement('button');
+  updateButton.type = 'button';
+  updateButton.classList.add('updateButton');
+  updateButton.textContent = 'タスク更新';
+  updateButton.addEventListener('click', () => updateTask(task.id));
+
+  taskSet.appendChild(taskTitle);
+  taskSet.appendChild(completed);
+  taskSet.appendChild(label);
+  taskSet.appendChild(newValueInput);
+  taskSet.appendChild(updateButton);
+
+  return taskSet;
+}
