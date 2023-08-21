6adcd9ba9c1c25454b18454e9b67e6a1b6d1c026
shuya goto <f20065@g.ichinoseki.ac.jp>
2023-08-21 11:30:32 +0900

    mockのデータと型の定義

---
+++ mock/mock.ts
@@ -0,0 +1,76 @@
+import type { Task, Tasks } from "../task.type";
+
+const taskListMock: Tasks[] = [
+        {
+            id: 0,
+            user: "shuya",
+            completed: 80,
+            tasks: [ // あとで実装するかも
+                {
+                    id: 0,
+                    name: "漢字どりる40ページまで",
+                    isCompleted: true
+                }, 
+                {
+                    id: 1,
+                    name: "漢字どりる80ページまで",
+                    isCompleted: false
+                },
+            ]
+        },
+        {
+            id: 1,
+            user: "ooi",
+            completed: 95.4,
+            tasks: [ // あとで実装するかも
+                {
+                    id: 0,
+                    name: "漢字どりる40ページまで",
+                    isCompleted: true
+                }, 
+                {
+                    id: 1,
+                    name: "漢字どりる80ページまで",
+                    isCompleted: false
+                },
+            ]
+        },
+	];
+
+const tasksMockUser0: Tasks = {
+    id: 0,
+    user: "shuya",
+    completed: 80,
+    tasks: [
+        {
+            id: 0,
+            name: "漢字どりる40ページまで",
+            isCompleted: true
+        }, 
+        {
+            id: 1,
+            name: "漢字どりる80ページまで",
+            isCompleted: false
+        },
+    ]
+};
+
+const tasksMockUser1: Tasks = {
+    id: 1,
+    user: "ooi",
+    completed: 95,
+    tasks: [
+        {
+            id: 0,
+            name: "漢字どりる40ページまで",
+            isCompleted: true
+        }, 
+        {
+            id: 1,
+            name: "漢字どりる80ページまで",
+            isCompleted: true
+        },
+    ]
+}
+
+export { taskListMock, tasksMockUser0, tasksMockUser1 };
