8490103bd82be41432f3e2abada1d79fbc497a54
shuya goto <f20065@g.ichinoseki.ac.jp>
2023-08-22 17:12:06 +0900

    getTaskを実装

--- task/controller.ts
+++ task/controller.ts
@@ -10,6 +10,30 @@ export class TaskControllerImpl implements TaskController{
 	constructor(private taskModel: TaskModel) {
 		this.taskModel = taskModel;
 	}
+	public async getTask(userId: number): Promise<string> {
+		const task = await this.taskModel.getUserTasks(userId);
+
+		if (!task.rows || task.rows.length === 0) {
+			return JSON.stringify({userId, task: []});
+		}
+
+		// is_completedがtrueの割合を求める
+		const completed = task.rows.filter((row) => row.is_completed).length / task.rows.length * 100;
+
+		const taskList = {
+			user_id: userId,
+			completed: completed,
+			tasks: task.rows.map((row) => {
+				return {
+					id: row.id,
+					name: row.description,
+					is_completed: row.is_completed,
+				};
+			}),
+		};
+		console.log(taskList);
+		return JSON.stringify(taskList);
+	}
 
 	public async updateTask(userId: number, TaskId: number, isCompleted: boolean): Promise<string> {
 		const task = await this.taskModel.updateTask(userId, TaskId, isCompleted);
