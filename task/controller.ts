c5dffd0a47e010f20fd46193b829676cce2f167e
shuya goto <f20065@g.ichinoseki.ac.jp>
2023-08-22 17:12:29 +0900

    getTasksを実装

--- task/controller.ts
+++ task/controller.ts
@@ -10,6 +10,38 @@ export class TaskControllerImpl implements TaskController{
 	constructor(private taskModel: TaskModel) {
 		this.taskModel = taskModel;
 	}
+
+	public async getTasks(userId: number): Promise<string> {
+		const tasks = await this.taskModel.getAllTasks();
+
+		if (!tasks.rows || tasks.rows.length === 0) {
+			return JSON.stringify({userId, tasks: []});
+		}
+
+		// user_idのユニークな数を取得する
+		const userIds = tasks.rows.map((row) => row.user_id);
+		const uniqueUserIds = userIds.filter((x, i, self) => self.indexOf(x) === i);
+
+		const userTaskList = [];
+		// user_idごとにis_completedがtrueの割合を求める
+		for (let i = 0; i < uniqueUserIds.length; i++) {
+			const userTask = tasks.rows.filter((row) => row.user_id === uniqueUserIds[i]);
+			const completed = userTask.filter((row) => row.is_completed).length / userTask.length * 100;
+			userTaskList.push({
+				user_id: uniqueUserIds[i],
+				completed: completed,
+				tasks: userTask.map((row) => {
+					return {
+						id: row.id,
+						name: row.description,
+						is_completed: row.is_completed,
+					};
+				}),
+			});
+		}
+		return JSON.stringify(userTaskList);
+	}
+
 	public async getTask(userId: number): Promise<string> {
 		const task = await this.taskModel.getUserTasks(userId);
 
