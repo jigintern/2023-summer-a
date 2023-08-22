364d6aa8925ca78a559d59dae4e84d1364160820
shuya goto <f20065@g.ichinoseki.ac.jp>
2023-08-22 17:13:08 +0900

    型の定義

---
+++ task/db.ts
@@ -0,0 +1,8 @@
+import { Client, ExecuteResult } from "https://deno.land/x/mysql@v2.11.0/mod.ts"
+import { Task, Tasks } from "../task.type.ts";
+
+export interface TaskModel {
+	getAllTasks(): Promise<ExecuteResult>;
+	getUserTasks(userId: number): Promise<ExecuteResult>;
+	updateTask(userId: number, taskId: number, isCompleted: boolean): Promise<ExecuteResult>;
+}
