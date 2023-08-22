b887ffe766edba6c9a05bec2434a90fd2e47a1f1
IOtomo <s2001024@sendai-nct.jp>
2023-08-22 09:47:10 +0900

    コメントを消して、チェックボックスが押されたときに、リクエストを送るように変更

--- public/update/code.js
+++ public/update/code.js
@@ -1,33 +1,7 @@
-// JSONデータの読み込み
-//import data from './test.json' assert { type: 'json' };
-
-// const response = await fetch("/tasks");
-// let jsonData = (await response.text());
-
-// const data = JSON.parse(jsonData);
-// console.log(jsonData.length);
-
-//         // プルダウンメニューにオプションを追加
-//         var userSelect = document.getElementById("userSelect");
-//         for (var i = 0; i < data.length; i++) {
-//             var option = document.createElement("option");
-//             option.value = data[i].user;
-//             option.text = data[i].user;
-//             userSelect.appendChild(option);
-//         }
-
-//                 // 変更ボタンにクリックイベントを追加し、func関数を実行
-//                 var changeButton = document.getElementById("changeButton");
-//                 changeButton.addEventListener("click", func);
-        
-//                 // 変更ボタンがクリックされたときに実行される空の関数
-//                 function changePage() {
-//                     Init(1);
-//                 }
-
 const userID=getParam('userID');
+
 if(userID){
-Init(userID);
+    Init(userID);
 }else
 {
     const updateResult = document.getElementById('updateResult');
@@ -36,47 +10,30 @@ Init(userID);
 
 async function Init(userID) 
  {
-const response = await fetch(`/tasks/${userID}`);
-let jsonData = (await response.text());
+    const response = await fetch(`/tasks/${userID}`);
+    let jsonData = (await response.text());
 
-const data = JSON.parse(jsonData);
+    const data = JSON.parse(jsonData);
+    console.log(data);
+    console.log(jsonData);
 
-console.log(data);
-console.log(jsonData);
+    // ユーザー情報を表示
+    const userNameElement = document.getElementById('userName');
+    userNameElement.textContent = data.user;
 
-// ユーザー情報を表示
-const userNameElement = document.getElementById('userName');
-userNameElement.textContent = data.user;
+    // ユーザーの進捗をプログレスバーで表示
+    const userProgress = document.getElementById('userProgress');
+    userProgress.value = data.completed;
 
-// ユーザーの進捗をプログレスバーで表示
-const userProgress = document.getElementById('userProgress');
-userProgress.value = data.completed;
+    const progressValue=document.getElementById('progressValue');
+    progressValue.textContent = String(data.completed);
 
-const progressValue=document.getElementById('progressValue');
-progressValue.textContent = String(data.completed);
-
-// JSONデータからタスクを動的に生成
-const taskList = document.getElementById('taskList');
-data.tasks.forEach((task) => {
-  const taskSet = createTaskSet(task);
-  taskList.appendChild(taskSet);
-});
-}
-
-// タスクを更新する関数
-function updateTask(taskNumber) {
-  // タスク内容の取得
-  const taskContent = document.querySelector(
-    `.taskSet:nth-child(${taskNumber}) .taskTitle`
-  ).textContent;
-
-  const newValue = document.querySelector(
-    `.taskSet:nth-child(${taskNumber}) .newValue`
-  ).checked;
-
-  // 更新結果を表示するエリアにメッセージを表示
-  const updateResult = document.getElementById('updateResult');
-  updateResult.textContent = `タスク "${taskContent}" が更新されました。新しい値: ${newValue}`;
+    // JSONデータからタスクを動的に生成
+    const taskList = document.getElementById('taskList');
+    data.tasks.forEach((task) => {
+      const taskSet = createTaskSet(task);
+      taskList.appendChild(taskSet);
+    });
 }
 
 // タスクセットを生成する関数
@@ -101,21 +58,17 @@ function createTaskSet(task) {
   newValueInput.required = true;
   newValueInput.checked=task.isCompleted;
 
-  const updateButton = document.createElement('button');
-  updateButton.type = 'button';
-  updateButton.classList.add('updateButton');
-  updateButton.textContent = 'タスク更新';
-  updateButton.addEventListener('click', () => updateTask(task.id+1));
-
   taskSet.appendChild(taskTitle);
   taskSet.appendChild(isCompleted);
   taskSet.appendChild(label);
   taskSet.appendChild(newValueInput);
-  taskSet.appendChild(updateButton);
+
+  newValueInput.addEventListener('change', () => checkBoxChanged(task.id+1));
 
   return taskSet;
 }
 
+//パラメーターを取得する関数
 function getParam(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -125,3 +78,30 @@ function getParam(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
+
+// タスクのチェックボックスが変更されたときに実行される関数
+async function checkBoxChanged(taskNumber) {
+    // タスク内容の取得
+    const taskContent = document.querySelector(
+      `.taskSet:nth-child(${taskNumber}) .taskTitle`
+    ).textContent;
+  
+    const newValue = document.querySelector(
+      `.taskSet:nth-child(${taskNumber}) .newValue`
+    ).checked;
+  
+    // 更新結果を表示するエリアにメッセージを表示
+    const updateResult = document.getElementById('updateResult');
+    updateResult.textContent = `タスク "${taskContent}" の状態が変更されました。新しい値: ${newValue}`;
+
+    const response = await fetch("/test",{
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({id:taskNumber-1,isCompleted:newValue})
+      });
+
+    const res= await response.text()
+    document.getElementById("server_response").textContent = res;
+    console.log(res);
+  }
+  
