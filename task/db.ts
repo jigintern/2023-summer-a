69f221dd57429a7bc3dea6b9dd57c3ea3e2a9885
shuya goto <f20065@g.ichinoseki.ac.jp>
2023-08-23 11:17:36 +0900

    user_nameを追加

--- task/db.ts
+++ task/db.ts
@@ -14,7 +14,9 @@ export class TaskDb implements TaskModel {
 	}
 
 	public async getAllTasks(): Promise<ExecuteResult> {
-		const userTasks = await this.db.execute(`SELECT * FROM user_tasks LEFT JOIN tasks ON user_tasks.Task_id = tasks.id;`);
+		const userTasks = await this.db.execute(`SELECT user_tasks.user_id, user_tasks.Task_id, user_tasks.is_completed, tasks.description, users.user_name 
+		FROM user_tasks LEFT JOIN tasks ON user_tasks.Task_id = tasks.id
+		RIGHT JOIN users ON users.user_id = user_tasks.user_id;`);
 
 		if (!userTasks.rows || userTasks.rows.length === 0) {
 			return {rows: []};
@@ -27,7 +29,9 @@ export class TaskDb implements TaskModel {
 	public async getUserTasks(userId: number): Promise<ExecuteResult> {
 		// user_tasksテーブルとtasksテーブルを結合して、user_idが一致するものを取得する
 		const task = await this.db.execute(
-			`SELECT * FROM user_tasks LEFT JOIN tasks ON user_tasks.Task_id = tasks.id WHERE user_tasks.user_id = ?`,
+			`SELECT user_tasks.user_id, user_tasks.Task_id, user_tasks.is_completed, tasks.description, users.user_name FROM user_tasks
+			LEFT JOIN tasks ON user_tasks.Task_id = tasks.id
+			RIGHT JOIN users ON user_tasks.id = users.user_id  WHERE user_tasks.user_id = ? ;`,
 			 [userId]
 			);
 
