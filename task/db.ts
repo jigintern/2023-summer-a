bd1b61467af48670f269321abaf8b0ba7ba428be
shuya goto <f20065@g.ichinoseki.ac.jp>
2023-08-23 13:02:36 +0900

    修正

--- task/db.ts
+++ task/db.ts
@@ -29,9 +29,9 @@ export class TaskDb implements TaskModel {
 	public async getUserTasks(userId: number): Promise<ExecuteResult> {
 		// user_tasksテーブルとtasksテーブルを結合して、user_idが一致するものを取得する
 		const task = await this.db.execute(
-			`SELECT user_tasks.user_id, user_tasks.Task_id, user_tasks.is_completed, tasks.description, users.user_name FROM user_tasks
-			LEFT JOIN tasks ON user_tasks.Task_id = tasks.id
-			RIGHT JOIN users ON user_tasks.id = users.user_id  WHERE user_tasks.user_id = ? ;`,
+			`SELECT user_tasks.user_id, user_tasks.Task_id, user_tasks.is_completed, tasks.description, users.user_name 
+			FROM user_tasks LEFT JOIN tasks ON user_tasks.Task_id = tasks.id
+			RIGHT JOIN users ON user_tasks.user_id = users.user_id WHERE user_tasks.user_id = ?;`,
 			 [userId]
 			);
 
