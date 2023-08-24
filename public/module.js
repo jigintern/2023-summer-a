30d5a348cb351326429f9f60bf0b839d7c3f97ee
t_ueda <74095322+Tatsuya-Ueda@users.noreply.github.com>
2023-08-24 15:01:15 +0900

    コメントアウトのいらない部分削除

--- public/module.js
+++ public/module.js
@@ -30,33 +30,6 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
 });
 
-// document.getElementById("load").onclick = async () => {
-//   let res;
-//   try {
-//     res = await fetchWithDidFromLocalstorage("/tasks", {
-//       method: "POST",
-//     });
-//   } catch (e) {
-//     console.log(e);
-//   }
-//   const json = await res.json();
-
-//   if (!res.ok) {
-//     // サーバーから成功ステータスが返ってこない場合(もしくはlocalStorageに認証情報がない)
-//     if (res.status == 303) {
-//       // localStorageにアカウント情報がない場合，303が返ってくるのでリダイレクト
-//       console.log(json.message);
-//       alert("ログインが必要です．ログインページへリダイレクトします．");
-//       location.href = json.redirectURL; // リダイレクトを実行
-//     } else {
-//       console.log(json);
-//     }
-//   } else {
-//     // 成功の応答が返ってきた場合
-//     console.log(json);
-//   }
-// };
-
 // document.getElementById("logout").onclick = () => {
 //   localStorage.clear();
 //   location.href = "/";
